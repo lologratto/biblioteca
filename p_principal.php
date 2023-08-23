88fa402e644a2dab99b840df1b5e666d4119a07e
jere <jeremiashornos14@gmail.com>
2023-08-23 10:55:04 -0300

    buscar libros pagina publica

--- p_principal.php
+++ p_principal.php
@@ -52,7 +52,8 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <li><a class="nav-link scrollto" href="#about">Libros</a></li>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
   Iniciar Sesion
 </button>
         </ul>
@@ -86,7 +87,68 @@
     <section id="about" class="about">
     <div class="container text-center" data-aos="fade-up" style="border: 1px #000;">
         
+    <div class="row">
+        <div class="section-title">
+          <h2>Libros</h2>
+          
+        </div>
+          <div class="col">
+            
+          </div>
+        </div>
+      </div>
+      <br>
+      <div>
+      <div class="caja3 ">
+          
         
+        
+        <table class="table caja2" border="2px"> 
+     </div >
+        
+
+        <table class="table table-bordered border-dark table-secondary table-hover" border="2px">
+        <thead class="m-1">
+      
+        <thead>
+          <tr>
+          <th scope="col">#</th>
+      <th scope="col">Nombre del libro</th>
+      <th scope="col">Editorial</th>
+      <th scope="col">Autor</th>
+      <th scope="col">Genero</th>
+      <th scope="col">Pais del autor</th>
+      <th scope="col">Numero de pagina</th>
+      <th scope="col">Año de edicion</th>
+      <th scope="col">Precio del libro</th>
+      <th scope="col">Stock</th>
+      <th scope="col">Estado del libro</th>
+          </tr>
+        </thead>
+        <tbody>
+<?php
+  require "php/conexion.php";
+  $q="select * from libros ";
+  $r=mysqli_query($con, $q);
+ 
+  while( $datos=mysqli_fetch_array($r)){
+    echo("<tr>");
+    echo("<td scope='row'> <a href='procesos/libros/libro2.php?id_libro=".$datos['id_libro']."&funcion=M' ><img src='img/editar.png' alt='' ></a>
+  
+  <a href='procesos/libros/libro2.php?id_libro=".$datos['id_libro']."&funcion=B'><img src='img/goma-de-borrar.png' alt=''></a>
+  
+  <td>". $datos['nombrel']."</td><td>".$datos['editorial']."</td><td>".$datos['autor']."</td><td>".$datos['genero']."</td><td>".$datos['pais_autor']."</td><td>".$datos['n_pagina']."</td><td>".$datos['año_edicion']."</td><td>".$datos['precio_libro']."</td><td>".$datos['stock']."</td><td>".$datos['estado']."</td>");
+   echo("</tr>");
+   
+
+
+  }
+  
+  ?>
+  </tbody>
+  <a href='procesos/libros/libro2.php?id_libro=0&funcion=A' ><img src='img/boton-agregar.png ' class="m-2" height="50px" ></a>
+  </table>
+</body>
 
       </div>
     </section><!-- End About Section -->
